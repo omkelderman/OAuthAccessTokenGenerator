2ac7d2952d8b2d3de90ffe8c896d101e6e309285
Olle Kelderman <omkelderman@gmail.com>
2020-06-12 16:50:32 +0200

    make things a bit more flexible

--- OAuthAccessTokenGenerator/Program.cs
+++ OAuthAccessTokenGenerator/Program.cs
@@ -2,45 +2,68 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace OAuthAccessTokenGenerator
 {
     public static class Program
     {
-        private static string CallbackUrl = "/oauth-cb";
+        private const string DefaultCallbackUrlLocalPath = "/oauth-cb";
+        private const string OsuOAuthBaseUrl = "https://osu.ppy.sh/oauth";
+        private const string DefaultDataIniFileName = "data.ini";
 
         public static void Main(string[] args)
         {
             // two things it can do: get new access token, or refresh existing one
             // if a data.ini file exists, use that and refresh
             // if not exists, create new
-            var data = OAuthData.TryReadData("data.ini");
-            var t = data == null ? DoNew() : DoExisting(data);
+            var dataIniFileName = args.FirstOrDefault() ?? DefaultDataIniFileName;
+            var data = OAuthData.TryReadData(dataIniFileName);
+            var t = data == null ? DoNew(dataIniFileName) : DoExisting(data, dataIniFileName);
             t.GetAwaiter().GetResult();
         }
 
-        private static async Task DoNew()
+        private static async Task DoNew(string dataIniFile)
         {
-            var portStr = Prompt("Local free port (leave empty to pick at random)");
+            Console.WriteLine($"No {dataIniFile} file found, generating a new one!");
+
+            // port
+            var portStr = Prompt("Local free port to use (leave empty to pick at random)");
             var port = string.IsNullOrWhiteSpace(portStr) ? GetRandomFreePort() : int.Parse(portStr);
-            var callbackUrl = GetCallbackUrlForPort(port);
-            Console.WriteLine("Create a new OAuth application with the following callback/redirect url: " +
-                              callbackUrl);
-            var baseUrl = Prompt("OAuth Base Url");
+
+            // local callback url
+            var defaultCallbackUrl = GetCallbackUrlForPort(port, DefaultCallbackUrlLocalPath);
+            Console.WriteLine(
+                $"Lets make up a callback/redirect url, enter if if you already have one or leave empty to use {defaultCallbackUrl}");
+            var callbackUrlLocalPath = Prompt(GetCallbackUrlForPort(port, "/"), false);
+            string callbackUrl;
+            if (string.IsNullOrWhiteSpace(callbackUrlLocalPath))
+            {
+                callbackUrl = defaultCallbackUrl;
+                callbackUrlLocalPath = DefaultCallbackUrlLocalPath;
+            }
+            else
+            {
+                callbackUrl = GetCallbackUrlForPort(port, $"/{callbackUrlLocalPath.Trim()}");
+            }
+
+            Console.WriteLine(
+                $"Create a new OAuth application with the following callback/redirect url: {callbackUrl}");
+
+            // oauth app details
+            var baseUrl = Prompt("OAuth Base Url", OsuOAuthBaseUrl);
             var clientId = Prompt("OAuth Client ID");
             var clientSecret = Prompt("OAuth Client Secret");
             var scopes = Prompt("OAuth scopes");
             var randomState = Guid.NewGuid().ToString();
             var authUrl = BuildAuthUrl(baseUrl, clientId, callbackUrl, scopes, randomState);
-            var listener = new OAuthLocalhostCallbackListener(port, randomState, CallbackUrl);
+            var listener = new OAuthLocalhostCallbackListener(port, randomState, callbackUrlLocalPath);
             listener.Start();
             Console.WriteLine(
                 "opening a browser window, follow steps there till it tells you to close the thing again and then come back here");
@@ -61,19 +84,30 @@ namespace OAuthAccessTokenGenerator
                 AccessToken = token.AccessToken,
                 RefreshToken = token.RefreshToken
             };
-            await File.WriteAllLinesAsync("data.ini", data.ToLines());
-            Console.WriteLine("Done!");
+            await DoEnding(data, dataIniFile);
         }
 
-        private static async Task DoExisting(OAuthData data)
+        private static string GetCallbackUrlForPort(in int port, string callbackUrlLocalPath) =>
+            port == 80 ? "http://localhost/callback" : $"http://localhost:{port}{callbackUrlLocalPath}";
+
+        private static async Task DoExisting(OAuthData data, string dataIniFile)
         {
+            Console.WriteLine($"{dataIniFile} file found!");
+            var doRefresh = PromptBool("Do refresh?");
+            if (!doRefresh) return;
             Console.WriteLine("Refreshing oauth token...");
             var token = await GetTokenFromRefreshToken(data.OAuthBaseUrl, data.OAuthClientId, data.OAuthClientSecret,
                 data.RefreshToken, data.Scopes);
             data.AccessToken = token.AccessToken;
             data.RefreshToken = token.RefreshToken;
-            await File.WriteAllLinesAsync("data.ini", data.ToLines());
-            Console.WriteLine("Done!");
+            await DoEnding(data, dataIniFile);
+        }
+
+        private static async Task DoEnding(OAuthData data, string dataIniFile)
+        {
+            await File.WriteAllLinesAsync(dataIniFile, data.ToLines());
+            Console.WriteLine($"Data saved to {dataIniFile}!");
+            Console.WriteLine("Look in that file for the access token and refresh token");
         }
 
         private static Task<OAuthTokenResponse> GetTokenFromAuthCode(string baseUrl, string clientId,
@@ -150,13 +184,40 @@ namespace OAuthAccessTokenGenerator
             return $"{baseUrl}token";
         }
 
-        private static string GetCallbackUrlForPort(in int port) =>
-            port == 80 ? "http://localhost/callback" : $"http://localhost:{port}{CallbackUrl}";
-
-        private static string Prompt(string prompt)
+        private static string Prompt(string prompt, bool appendColon = true)
         {
-            Console.Write(prompt + ": ");
+            Console.Write(prompt);
+            if (appendColon)
+            {
+                Console.Write(": ");
+            }
+
             return Console.ReadLine();
+        }
+
+        private static string Prompt(string prompt, string defaultValue)
+        {
+            Console.Write($"{prompt} [{defaultValue}]: ");
+            var answer = Console.ReadLine();
+            return string.IsNullOrWhiteSpace(answer) ? defaultValue : answer;
+        }
+
+        private static bool PromptBool(string prompt)
+        {
+            var str = $"{prompt}? [y/n] ";
+            while (true)
+            {
+                var response = Prompt(str, false).FirstOrDefault();
+                switch (response)
+                {
+                    case 'y':
+                    case 'Y':
+                        return true;
+                    case 'n':
+                    case 'N':
+                        return false;
+                }
+            }
         }
 
         private static int GetRandomFreePort()
